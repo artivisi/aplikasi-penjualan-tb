fa21d6264897be47f2f9c4837e462e5c4f53ed9a
Jimmy Rengga <j.rengga@gmail.com>
2014-05-14 15:18:42 +0700

    tambah config hibernate dan interface service

---
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/service/AppService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.smktarunabhakti.penjualan.service;
+
+import net.smktarunabhakti.penjualan.domain.Barang;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ *
+ * @author jimmy
+ */
+public interface AppService {
+    
+    void simpanBarang(Barang b);
+    
+    void hapusBarang(Barang b);
+    
+//    Page<Barang> cariSemuaBarang(Pageable p);
+    
+    Barang cariBarangById(String id);
+    
+    Long countSemuaBarang();
+    
+}
