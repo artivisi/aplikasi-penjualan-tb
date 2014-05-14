d7b47d8cf2866df7ce5c5805f1e552059047d8ac
Jimmy Rengga <j.rengga@gmail.com>
2014-05-14 09:52:52 +0700

    tambah depedency untuk hibernate dan log

---
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -0,0 +1,13 @@
+package net.smktarunabhakti.penjualan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
