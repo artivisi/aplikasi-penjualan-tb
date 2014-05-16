fcb0ec61aa78f1fd34b21e9d53afe886993bd097
Jimmy Rengga <j.rengga@gmail.com>
2014-05-16 21:14:44 +0700

    service simpan barang dari main

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -1,14 +1,28 @@
 package net.smktarunabhakti.penjualan;
 
+import net.smktarunabhakti.penjualan.domain.Barang;
+import net.smktarunabhakti.penjualan.service.AppService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     private static ApplicationContext appCtx;
+    private static AppService appService;
 
     public static void main(String[] args) {
         appCtx = new ClassPathXmlApplicationContext(
                 "classpath:net/smktarunabhakti/penjualan/applicationContext.xml");
+        
+        appService = (AppService) appCtx.getBean("appService");
+        saveBarang();
+    }
+    
+    private static void saveBarang() {
+        Barang b = new Barang();
+        b.setKodeBarang("test1");
+        b.setNamaBarang("nama");
+        
+        appService.simpanBarang(b);
     }
 }
