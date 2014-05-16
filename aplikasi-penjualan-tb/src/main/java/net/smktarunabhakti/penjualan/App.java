524bbaaaf56644feff1631aa4e178473d02ce2f3
Jimmy Rengga <j.rengga@gmail.com>
2014-05-16 14:44:40 +0700

    fix hibernate.cfg.xml, pom.xml

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -1,13 +1,14 @@
 package net.smktarunabhakti.penjualan;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+    private static ApplicationContext appCtx;
+
+    public static void main(String[] args) {
+        appCtx = new ClassPathXmlApplicationContext(
+                "classpath:net/smktarunabhakti/penjualan/applicationContext.xml");
     }
 }
