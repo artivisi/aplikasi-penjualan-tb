34688ad6038d9d6918e335836e2ba8cece008a2e
Jimmy Rengga <j.rengga@gmail.com>
2014-05-19 15:25:20 +0700

    initial android maven project

---
+++ penjualan-android/src/main/java/net/smktarunabhakti/domain/Barang.java
@@ -0,0 +1,37 @@
+package net.smktarunabhakti.domain;
+
+/**
+ * Created by jimmy on 19/05/14.
+ */
+public class Barang {
+
+    private String id;
+
+    private String kodeBarang;
+
+    private String namaBarang;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKodeBarang() {
+        return kodeBarang;
+    }
+
+    public void setKodeBarang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+}
---
+++ penjualan-android/src/main/java/net/smktarunabhakti/service/AndroidService.java
@@ -0,0 +1,9 @@
+package net.smktarunabhakti.service;
+
+/**
+ * Created by jimmy on 19/05/14.
+ */
+public class AndroidService {
+
+
+}
